cb43b9df427895f7f5896580d5b42f5618ee571b
Daniel Brendel <dbrendel1988@gmail.com>
2022-08-05 02:55:02 +0200

    Fix structure

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * SteamCards node package index file
+ */
+
+import './src/css/steamcards.css';
+
+require('./src/js/element.js');
+
+window.SteamCard = require('./src/js/card.js');
+
--- src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * SteamCards node package index file
- */
-
-import 'css/steamcards.css';
-
-require('js/steamcards.js');
-
